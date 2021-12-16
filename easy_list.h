5112add13c5518b70b9a2713d19ac395c2df9b30
ibkov <34207589+ibkov@users.noreply.github.com>
2021-12-16 12:01:13 +0300

    213

--- easy_list.h
+++ easy_list.h
@@ -1,4 +1,4 @@
 #pragma once
 #include <vector>
 
-void itc_even_index_list (const vector <int> &mass, vector <int> &mass2);
+void itc_even_index_list (const std::vector <int> &mass, std::vector <int> &mass2);
