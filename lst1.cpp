3ffa94c8a6af9272bf3a9619b99a19b2cfad420e
ibkov <34207589+ibkov@users.noreply.github.com>
2021-12-16 10:35:14 +0300

    cr
    dads

---
+++ lst1.cpp
@@ -0,0 +1,8 @@
+void itc_even_index_list(const vector <int> &mass, vector <int> &mass2)
+{
+  if (!mass || !mass2)
+    return;
+  for (int i == 0; i < mass.size(); i++)
+    if (mass[i] % 2 == 0)
+      mass2.push_back(mass[i]);
+}
